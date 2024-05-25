27629b53173dedcd8dd591e9fb33415bf9d78592
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-05-25 14:50:23 -0600

    Add validation for city name in Point class constructor

--- code/point.py
+++ code/point.py
@@ -3,7 +3,11 @@
 
 class Point:
     """A point on the Earth's surface."""
+
     def __init__(self, name, lattitude, longitude):
+        if not isinstance(name, str):
+            raise ValueError(f"Invalid city name. Must be a string and not of type: {type(name)}")
+
         self.name = name
 
         if not (-90 <= lattitude <= 90) or not (-180 <= longitude <= 180):
