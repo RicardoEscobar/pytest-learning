13e8ca6db50c1cafcb9bd9914ab84eaa9d01434e
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-10-21 03:00:27 -0600

    Refactor factory fixtures to use directory path instead of file path

--- tests/test_factory_fixtures.py
+++ tests/test_factory_fixtures.py
@@ -1,38 +1,53 @@
 """Testing factory fixtures for the application."""
+import os
+import csv
+import json
+
 import pytest
 
 
 @pytest.fixture(scope='function')
-def file_path():
+def dir_path():
     """Return the path to the file."""
-    return 'test_data.txt'
+    return 'data/'
 
 
 @pytest.fixture(scope='function')
-def data(file_path):
+def process_data(dir_path):
     """Return the data to be used for testing."""
-    def _data(lines_to_read: int = 1):
-        with open(file_path, 'r') as file:
-            data = file.readlines()[:lines_to_read]
-        return data
+    files = os.listdir(dir_path)
+
+    def _specify_type(filename_or_type: str):
+        for file in files:
+            if filename_or_type in file:
+                if filename_or_type.endswith('.txt'):
+                    with open(f'{dir_path}{file}', 'r') as file:
+                        return file.readlines()
+                elif filename_or_type.endswith('.csv'):
+                    with open(f'{dir_path}{file}', 'r') as file:
+                        return list(csv.reader(file))
+                elif filename_or_type.endswith('.json'):
+                    with open(f'{dir_path}{file}', 'r') as file:
+                        return json.load(file)
+        raise FileNotFoundError(f'File {filename_or_type} not found.')
+                
     
-    return _data
+    return _specify_type
 
 
-def test_file_path(file_path, data):
+def test_file_path(dir_path, process_data):
     """Test the file path."""
-    assert file_path == 'test_data.txt'
+    assert dir_path == 'test_data.txt'
 
     # Create a new file
-    with open(file_path, 'w') as file:
+    with open(dir_path, 'w') as file:
         for i in range(1, 11, 1):
             file.write(f'Hello {i} times.\n')
     
     # Load the data
-    loaded_data = data()
+    loaded_data = process_data()
     assert loaded_data == ['Hello 1 times.\n']
 
     # Load multiple lines
-    loaded_data = data(3)
+    loaded_data = process_data(3)
     assert loaded_data == ['Hello 1 times.\n', 'Hello 2 times.\n', 'Hello 3 times.\n']
-
