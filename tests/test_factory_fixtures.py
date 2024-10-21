89dfae52baddf54695282f61b73ac8c7286c4fc8
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-10-21 04:47:05 -0600

    Refactor factory fixtures to use directory path instead of file path

--- tests/test_factory_fixtures.py
+++ tests/test_factory_fixtures.py
@@ -1,38 +1,8 @@
 """Testing factory fixtures for the application."""
 
 import os
-import csv
-import json
 
 import pytest
-
-
-@pytest.fixture(scope="module")
-def dir_path():
-    """Return the path to the file."""
-    return "data/"
-
-
-@pytest.fixture(scope="module")
-def process_data(dir_path):
-    """Return the data to be used for testing."""
-    files = os.listdir(dir_path)
-
-    def _specify_type(filename_or_type: str):
-        for file in files:
-            if filename_or_type in file:
-                if filename_or_type.endswith(".txt"):
-                    with open(f"{dir_path}{file}", "r") as file:
-                        return file.readlines()
-                elif filename_or_type.endswith(".csv"):
-                    with open(f"{dir_path}{file}", "r") as file:
-                        return list(csv.reader(file))
-                elif filename_or_type.endswith(".json"):
-                    with open(f"{dir_path}{file}", "r") as file:
-                        return json.load(file)
-        raise FileNotFoundError(f"File {filename_or_type} not found.")
-
-    return _specify_type
 
 
 def test_file_path(dir_path, process_data):
